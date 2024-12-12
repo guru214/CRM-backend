04e8382903d88cb943a617af708cdcd846efc6af
anwaaransari12 <anwaar.ahmed@bitstek.io>
2024-12-12 11:05:41 +0530

    userControllers now will connect and disconnect the sqlDB

---
+++ config/sqldb.js
@@ -0,0 +1,34 @@
+import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
+
+dotenv.config(); // Load environment variables
+
+// Create a new Sequelize instance
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: 'mysql', // Specify the dialect for MySQL
+  logging: false, // Disable logging; default: console.log
+  port: process.env.DB_PORT
+});
+
+// Function to open the connection
+const openConnection = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+};
+
+// Function to close the connection
+const closeConnection = async () => {
+  try {
+    await sequelize.close();
+    console.log('Connection has been closed successfully.');
+  } catch (error) {
+    console.error('Error closing the connection:', error);
+  }
+};
+
+export { openConnection, closeConnection, sequelize };
