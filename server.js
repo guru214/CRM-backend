d76f45c109516960a5f4d93317fb6c7bf4ae0f54
Anwaar Ansari <anwaar.ahmed@bitstek.io>
2024-12-30 18:31:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 app.use(cors({
-  origin: '*', // Allow only the frontend URL
+  origin: 'https://localhost:3000/', // Allow only the frontend URL
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   credentials: true,
 }));
