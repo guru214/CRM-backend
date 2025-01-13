1cf23bc263180e89224d65584f483738473b3fba
Anwaar Ansari <anwaar.ahmed@bitstek.io>
2025-01-13 19:04:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 app.use(cors({
-  origin: ['https://crm-frontend-git-anwaar-anwaar-ansari12s-projects.vercel.app', 'https://localhost:3000'], // Allow only the frontend URL
+  origin: ['https://richesse-crm.vercel.app', 'https://localhost:3000'], // Allow only the frontend URL
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   credentials: true,
 }));
