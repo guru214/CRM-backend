0a3feaa3ad16e24d20698800d5353f48c850798e
Anwaar Ansari <anwaar.ahmed@bitstek.io>
2025-01-13 18:57:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 app.use(cors({
-  origin: ['https://crm-frontend-mauve-six.vercel.app', 'https://localhost:3000'], // Allow only the frontend URL
+  origin: ['https://crm-frontend-git-anwaar-anwaar-ansari12s-projects.vercel.app', 'https://localhost:3000'], // Allow only the frontend URL
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   credentials: true,
 }));
